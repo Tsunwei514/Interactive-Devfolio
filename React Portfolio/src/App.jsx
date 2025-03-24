a545ac7e7ff5a00c894910324caec834950dff7c
Tsunwei514 <tristin.tsun@gmail.com>
2025-03-24 10:34:52 -0600

    setup project router connection and header navigation

---
+++ React Portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Nav from './components/NavTabs.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ React Portfolio/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Error from './pages/Error.jsx'
+import About from './pages/About.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Contact from './pages/Contact.jsx'
+import Resume from './pages/Resume.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+);
